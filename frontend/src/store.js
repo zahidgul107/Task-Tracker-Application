0ad8352cfe88ff587d341584c2f1843450139fb7
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-02-25 21:56:56 +0530

    added login slice and other

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,7 @@
-import React from 'react'
 import { configureStore } from '@reduxjs/toolkit'
-
+import loginReducer from './features/login/loginSlice'
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    login: loginReducer,
+  },
 })
